e2461b671d92e2a4b8862041be3c84e2ada424a2
moshi <sy.41211@gmail.com>
2021-10-18 11:43:57 +0900

    Fix test code to match origin code changes

--- tests/util/test_genbank.py
+++ tests/util/test_genbank.py
@@ -17,7 +17,9 @@ def test_is_valid_format_ng(blank_file: Path):
 def test_convert_cds_fasta_protein(genbank_file: Path, tmp_path: Path):
     """test convert cds_fasta (seqtype=protein)"""
     cds_protein_fasta_outfile = tmp_path / "cds.fa"
-    UtilGenbank(genbank_file).convert_cds_fasta(cds_protein_fasta_outfile, "protein")
+    UtilGenbank(genbank_file).convert_cds_fasta(
+        cds_protein_fasta_outfile, "protein", "prefix"
+    )
     assert len(list(SeqIO.parse(cds_protein_fasta_outfile, "fasta"))) != 0
 
 
@@ -25,6 +27,6 @@ def test_convert_cds_fasta_nucleotide(genbank_file: Path, tmp_path: Path):
     """test convert cds_fasta (seqtype=nucleotide)"""
     cds_nucleotide_fasta_outfile = tmp_path / "cds.fa"
     UtilGenbank(genbank_file).convert_cds_fasta(
-        cds_nucleotide_fasta_outfile, "nucleotide"
+        cds_nucleotide_fasta_outfile, "nucleotide", "prefix"
     )
     assert len(list(SeqIO.parse(cds_nucleotide_fasta_outfile, "fasta"))) != 0
