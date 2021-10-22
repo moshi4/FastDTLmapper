1eb37a716da9fe5fdfa9c426a60c003a05b205c2
moshi <sy.41211@gmail.com>
2021-10-22 13:45:13 +0900

    Add test for resolve multifuracation method

--- tests/util/test_tree.py
+++ tests/util/test_tree.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from ete3 import Tree
 from fastdtlmapper.util import UtilTree
 
 
@@ -41,3 +42,32 @@ def test_make_3gene_tree(tmp_path: Path):
     UtilTree.make_3genes_tree(fasta_3gene_file, gene_tree_outfile)
     gene_tree_text = gene_tree_outfile.read_text()
     assert gene_tree_text == "(insp1:0.01,insp2:0.01,outsp1:0.01);"
+
+
+def test_multifurcate_zero_length_nodes(tmp_path: Path):
+    """test multifurcate_zero_length_nodes"""
+    root_tree_file = tmp_path / "root_tree.nwk"
+    root_tree_text = "(((a:0,b:0):0,c:0):1,(d:1,e:1):1);"
+    root_tree_file.write_text(root_tree_text)
+    root_tree = Tree(root_tree_text)
+
+    multifurcate_outfile = tmp_path / "multifuracate_tree.nwk"
+    UtilTree.multifurcate_zero_length_nodes(root_tree_file, multifurcate_outfile)
+    multifurcate_tree = Tree(multifurcate_outfile.read_text())
+
+    assert len(root_tree.get_common_ancestor("a", "b", "c").children) == 2
+    assert len(multifurcate_tree.get_common_ancestor("a", "b", "c").children) == 3
+
+
+def test_unroot_tree(tmp_path: Path):
+    """test unroot_tree"""
+    root_tree_file = tmp_path / "root_tree.nwk"
+    root_tree_text = "(((a,b),c),(d,e));"
+    root_tree_file.write_text(root_tree_text)
+
+    unroot_tree_file = tmp_path / "unroot_tree.nwk"
+    UtilTree.unroot_tree(root_tree_file, unroot_tree_file)
+    unroot_tree = Tree(unroot_tree_file.read_text())
+
+    assert len(Tree(root_tree_text).get_tree_root().children) == 2
+    assert len(unroot_tree.children) == 3
