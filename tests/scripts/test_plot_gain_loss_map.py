57ef39b781b79d66dec0bff3315beffe1826b93b
moshi <41852815+moshi4@users.noreply.github.com>
2021-10-23 19:09:23 +0900

    Feat #62 add brn/dup/trn plot (#63)
    * Add brn/dup/trn plot option
    
    * Fix test code & test data
    
    * Add --add_brn_dup_trn option test code

--- tests/scripts/test_plot_gain_loss_map.py
+++ tests/scripts/test_plot_gain_loss_map.py
@@ -5,19 +5,19 @@ from fastdtlmapper.scripts.plot_gain_loss_map import get_args, main
 
 
 def test_plot_gain_loss_map_default_param(
-    gain_loss_map_nwk_file: Path,
+    dtl_map_nwk_file: Path,
     tmp_path: Path,
 ):
     """test plot_gain_loss_map with default param"""
     outfile = tmp_path / "all_gain_loss_map.png"
-    argv = f"-i {gain_loss_map_nwk_file} -o {outfile}"
+    argv = f"-i {dtl_map_nwk_file} -o {outfile}"
     args = get_args(argv.split(" "))
     main(args)
     assert outfile.is_file()
 
 
 def test_plot_gain_loss_map_user_param(
-    gain_loss_map_nwk_file: Path,
+    dtl_map_nwk_file: Path,
     tmp_path: Path,
 ):
     """test plot_gain_loss_map with user param"""
@@ -25,8 +25,8 @@ def test_plot_gain_loss_map_user_param(
     # Perfect plot test required confirming figure contents
     # It's impossible, so Color,Symbol,FontSize options are ignored
     argv = (
-        f"-i {gain_loss_map_nwk_file} -o {outfile} --plot_margin 100 "
-        + "--plot_width 1000 --title helloworld --ladderize"
+        f"-i {dtl_map_nwk_file} -o {outfile} --plot_margin 100 "
+        + "--plot_width 1000 --title helloworld --ladderize --add_brn_dup_trn"
     )
     args = get_args(argv.split(" "))
     main(args)
@@ -34,12 +34,12 @@ def test_plot_gain_loss_map_user_param(
 
 
 def test_plot_gain_loss_map_ng_extension(
-    gain_loss_map_nwk_file: Path,
+    dtl_map_nwk_file: Path,
     tmp_path: Path,
 ):
     """test plot_gain_loss_map with outfile extension error"""
     outfile_invalid_ext = tmp_path / "all_gain_loss_map.invalid"
-    argv = f"-i {gain_loss_map_nwk_file} -o {outfile_invalid_ext}"
+    argv = f"-i {dtl_map_nwk_file} -o {outfile_invalid_ext}"
     with pytest.raises(SystemExit) as e:
         get_args(argv.split(" "))
     assert e.type == SystemExit
